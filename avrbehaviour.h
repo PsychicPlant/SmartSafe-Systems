234cfcd8afc69b9202cfcd03abd5b3b3b9f78331
PsychicPlant <guilhermeruas20@hotmail.com>
2023-04-02 18:45:09 +0100

    Everything ready for the implementation of comands. The program will interface not only with the USART but also with another input file which will be read by a child process. The child process will filter out input and send it to the main process through a pipe.
    This is the last itteration of the working code before these changes
    start to be implemented.

--- avrbehaviour.h
+++ avrbehaviour.h
@@ -124,8 +124,6 @@ int comb_process(char input, char *comb)
 		*(comb+i) = avrkey2str(input);
 		if(i == 5)
 		{
-//			i++;
-//			*(comb+i) = 0x0d;
 			return 0;
 		}
 	}
--- avrserial.c
+++ avrserial.c
@@ -1,4 +1,5 @@
 #include <sys/types.h>
+#include <errno.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <termios.h>
@@ -32,148 +33,103 @@ volatile int STOP=FALSE;
 
 int main(void)
 {
-    int fd1, res;
-  
-  //	fd1 stands for file descriptor, and it is a pointer to the open file that we will be inspecting, reading and writing to. The integer that it returns is an index number of the open file process.
-  
+    
+    int fd1, res, avrresp;
     struct termios oldtio,newtio;
-  
-  //		The termios structure is defined in the “asm/termbits.h” file and is used by the library to create all three models of serial com.
-  //		There are two termios structs being created here. One will contain the current device settings (default), before the program can do any changes to it, called oldtio. The newtio will serve to configure the new terminal connection.
-  
     char buf[255];
-    char comb_buf[8] = {0};
-    
-   int avrresp;
-    
-  /* 
-    Open modem device for reading and writing and not as controlling tty
-    because we don't want to get killed if linenoise sends CTRL-C.
-  */
-   fd1 = open(MODEMDEVICE, O_RDWR | O_NOCTTY );
-  
-  //		This opens the file with R/D permission & more, and returns an int value that will be stored in the fd1 variable.
-  
-   if (fd1 <0) {perror(MODEMDEVICE); exit(-1); }
-  
-  //		This will return an error in case the file could not be opened. It will then quit out of the program with the exit code -1.
-  
-   tcgetattr(fd1,&oldtio); /* save current serial port settings */
-  
-  //		The tcgetattr function will get the terminal parameters of the file opened, described by fd1, and save it into a termios struct. This is where we will use our oldtio.
-  
-   bzero(&newtio, sizeof(newtio)); /* clear struct for new port settings */
-  
-  
-  
-  //		The bzero function erases all bytes the sizeof specified object, starting at a memory location.
+    char comb_buf[7] = {0};
+/*
+    pid_t cpid;
+    int pipefd[2];
 
-  
-  
-  /* 
-    BAUDRATE: Set bps rate. You could also use cfsetispeed and cfsetospeed.
-    CRTSCTS : output hardware flow control (only used if the cable has
-              all necessary lines. See sect. 7 of Serial-HOWTO)
-    CS8     : 8n1 (8bit,no parity,1 stopbit)
-    CLOCAL  : local connection, no modem contol
-    CREAD   : enable receiving characters
-  */
-   newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
-  
-  //		There is no necessity for the CRTSCTS flag in our configuration, since we will not be using those communication pins.
-  
-  //PARENB is a value that will set the Even Parity. It will generate that parity on write and inspect it on read.
-  
-   
-  /*
-    IGNPAR  : ignore bytes with parity errors
-    ICRNL   : map CR to NL (otherwise a CR input on the other computer
-              will not terminate input)
-    otherwise make device raw (no other input processing)
-  */
-   newtio.c_iflag = IGNPAR | ICRNL;
-   
-  //		We would like to have parity in our system, and as such instead of IGNPAR, we will use INPCK.
-  
-  
-  /*
-   Raw output.
-  */
-   newtio.c_oflag = 0;
-   
-  /*
-    ICANON  : enable canonical input
-    disable all echo functionality, and don't send signals to calling program
-  */
-   newtio.c_lflag = ICANON;
-  
-  //		For testing, we will have the ECHO flag here.
-  //		This is an example of a Canonical model. In our Non-canonical, we will set this to 0.
-  
-  /* 
-    initialize all control characters 
-    default values can be found in /usr/include/termios.h, and are given
-    in the comments, but we don't need them here
-  */
-   newtio.c_cc[VINTR]    = 0;     /* Ctrl-c */ 
-   newtio.c_cc[VQUIT]    = 0;     /* Ctrl-\ */
-   newtio.c_cc[VERASE]   = 0;     /* del */
-   newtio.c_cc[VKILL]    = 0;     /* @ */
-   newtio.c_cc[VEOF]     = 4;     /* Ctrl-d */
-   newtio.c_cc[VTIME]    = 0;     /* inter-character timer unused */
-   newtio.c_cc[VMIN]     = 1;     /* blocking read until 1 character arrives */
-   newtio.c_cc[VSWTC]    = 0;     /* '\0' */
-   newtio.c_cc[VSTART]   = 0;     /* Ctrl-q */ 
-   newtio.c_cc[VSTOP]    = 0;     /* Ctrl-s */
-   newtio.c_cc[VSUSP]    = 0;     /* Ctrl-z */
-   newtio.c_cc[VEOL]     = 0;     /* '\0' */
-   newtio.c_cc[VREPRINT] = 0;     /* Ctrl-r */
-   newtio.c_cc[VDISCARD] = 0;     /* Ctrl-u */
-   newtio.c_cc[VWERASE]  = 0;     /* Ctrl-w */
-   newtio.c_cc[VLNEXT]   = 0;     /* Ctrl-v */
-   newtio.c_cc[VEOL2]    = 0;     /* '\0' */
-  
-  /* 
-    now clean the modem line and activate the settings for the port
-  */
-   tcflush(fd1, TCIFLUSH);
-   tcsetattr(fd1,TCSANOW,&newtio);
-  
-  /*
-    terminal settings done, now handle input
-    In this example, inputting a 'z' at the beginning of a line will 
-    exit the program.
-  */
-   
-   while (STOP==FALSE) {     /* loop until we have a terminating condition */
-   /* read blocks program execution until a line terminating character is 
-      input, even if more than 255 chars are input. If the number
-      of characters read is smaller than the number of chars available,
-      subsequent reads will return the remaining chars. res will be set
-      to the actual number of characters actually read */
-      res = read(fd1,buf,255); 
-      buf[res]=0;             /* set end of string, so we can printf */
-      char *bufpt = &buf[0];
-          
-      if (*bufpt == 0xF1)
-      {
-          avrresp = avrprocessing(bufpt, comb_buf);
-          if(avrresp == 1)
-          {
-              FILE * fd2 = fopen(COMBTXT, "a" ); 
-              if (fd2 <0) {perror(COMBTXT); exit(-1); }
-              
-              fprintf(fd2, "%s\n", comb_buf);
-              fclose(fd2);
-              memset(comb_buf, 0, sizeof comb_buf);
-          }
-      }
-      else 
-    
-      printf("%s", buf);
+    if (pipe(pipefd) == -1) 
+    {
+        perror("pipe");
+        exit(EXIT_FAILURE);
+    }
+
+    cpid = fork();
+    if (cpid == -1) {
+        perror("fork");
+        exit(EXIT_FAILURE);
+    }
+
+    if (cpid == 0)
+    {    // Child reads from pipe 
+        close(pipefd[1]);         
+
+    } 
+    else 
+    {
+        close(pipefd[0]);      
         
+    }
+*/
+
+    fd1 = open(MODEMDEVICE, O_RDWR | O_NOCTTY );
+    
+    if (fd1 <0) {perror(MODEMDEVICE); exit(-1); }
+    
+    tcgetattr(fd1,&oldtio); /* save current serial port settings */
+    
+    bzero(&newtio, sizeof(newtio)); 
+    
+    newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
+    newtio.c_iflag = IGNPAR | ICRNL;
+    newtio.c_oflag = 0;
+    newtio.c_lflag = ICANON;
+    newtio.c_cc[VINTR]    = 0;     /* Ctrl-c */ 
+    newtio.c_cc[VQUIT]    = 0;     /* Ctrl-\ */
+    newtio.c_cc[VERASE]   = 0;     /* del */
+    newtio.c_cc[VKILL]    = 0;     /* @ */
+    newtio.c_cc[VEOF]     = 4;     /* Ctrl-d */
+    newtio.c_cc[VTIME]    = 0;     /* inter-character timer unused */
+    newtio.c_cc[VMIN]     = 1;     /* blocking read until 1 character arrives */
+    newtio.c_cc[VSWTC]    = 0;     /* '\0' */
+    newtio.c_cc[VSTART]   = 0;     /* Ctrl-q */ 
+    newtio.c_cc[VSTOP]    = 0;     /* Ctrl-s */
+    newtio.c_cc[VSUSP]    = 0;     /* Ctrl-z */
+    newtio.c_cc[VEOL]     = 0;     /* '\0' */
+    newtio.c_cc[VREPRINT] = 0;     /* Ctrl-r */
+    newtio.c_cc[VDISCARD] = 0;     /* Ctrl-u */
+    newtio.c_cc[VWERASE]  = 0;     /* Ctrl-w */
+    newtio.c_cc[VLNEXT]   = 0;     /* Ctrl-v */
+    newtio.c_cc[VEOL2]    = 0;     /* '\0' */
+    
+    tcflush(fd1, TCIFLUSH);
+    tcsetattr(fd1,TCSANOW,&newtio);
+    
+    while (STOP==FALSE) {     
+    /* loop until we have a terminating condition */
+    /* read blocks program execution until a line terminating character is 
+    input, even if more than 255 chars are input. If the number
+    of characters read is smaller than the number of chars available,
+    subsequent reads will return the remaining chars. res will be set
+    to the actual number of characters actually read */
+
+    res = read(fd1,buf,255); 
+    buf[res]=0;             /* set end of string, so we can printf */
+    char *bufpt = &buf[0];
+    
+    
+    if (*bufpt == 0xF1)
+    {
+      avrresp = avrprocessing(bufpt, comb_buf);
+      if(avrresp == 1)
+      {
+        FILE * fd2 = fopen(COMBTXT, "a" ); 
+        if (fd2 <0) {perror(COMBTXT); exit(-1); }
+        
+        fprintf(fd2, "%s\n", comb_buf);
+        fclose(fd2);
+        memset(comb_buf, 0, sizeof comb_buf);
+      }
+    }
+    else printf("%s", buf);
+    
     //if (buf[0]=='z') STOP=TRUE;
     }
     /* restore the old port settings */
     tcsetattr(fd1,TCSANOW,&oldtio);
 }
+  
